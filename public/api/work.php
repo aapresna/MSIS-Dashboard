2040bc1810076abca0acaa7489288083031c5f2d
aapresna <aapresna@indiana.edu>
2018-10-10 10:26:52 -0400

    Sorta updated

--- public/api/work.php
+++ public/api/work.php
@@ -1,38 +1,20 @@
 <?php
 
-echo '<pre>';
-$foo = "bar\n";
-$bar = 'bit\n';
-echo $foo;
-echo $bar;
-$one = 'ONE';
-echo "The value is {$one}.\n";
-echo "The value is " . $one;
-class Animal
-{
-  public $name = 'Pig';
+require '../../app/common.php';
+
+$taskId = intval($_GET['taskId'] ?? 0);
+
+if ($taskId < 1) {
+  throw new Exception('Invalid Task ID');
 }
-$wilber = new Animal();
-echo $wilber->name . "\n";
-$myArr = [
-  'first' => 'Tom',
-  'last' => 'Gregory'
-];
-$people = [
-  0 => 'Tom',
-  1 => 'Phil',
-  2 => 'Midge'
-];
-var_dump($myArr);
-print_r($people);
-/*
-$n = 0;
-if (isset($_GET['id'])) {
-  $n = $_GET['id']
-}
-// This means the same thing:
- $n = isset($_GET['id']) ? $_GET['id'] : 0;
-*/
- // This also means the same thing:
-$n = $_GET['id'] ?? 0;
-echo '$n = ' . $n ."\n";
+
+
+// 1. Go to the database and get all work associated with the $taskId
+$workArr = Work::getWorkByTaskId($taskId);
+
+// 2. Convert to JSON
+$json = json_encode($workArr, JSON_PRETTY_PRINT);
+
+// 3. Print
+header('Content-Type: application/json');
+echo $json;
